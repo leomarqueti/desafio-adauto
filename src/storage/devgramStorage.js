c35625ff38c1a7373884babd7febfcee80b6f9ff
Luis Guilherme Marques Andrade <lguilhermeandradem@gmail.com>
2026-05-08 20:28:50 -0300

    salvar usuario

--- src/storage/devgramStorage.js
+++ src/storage/devgramStorage.js
@@ -1,25 +1,79 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { STORAGE_KEYS } from './storageKeys';
 
-export async function salvarUsuario(usuario) {
-  await AsyncStorage.setItem(STORAGE_KEYS.USUARIO, JSON.stringify(usuario));
+export async function buscarUsuarios() {
+  const dados = await AsyncStorage.getItem(STORAGE_KEYS.USUARIOS);
+
+  return dados ? JSON.parse(dados) : [];
 }
 
-export async function buscarUsuario() {
-  const dados = await AsyncStorage.getItem(STORAGE_KEYS.USUARIO);
+export async function cadastrarUsuario(novoUsuario) {
+  const usuarios = await buscarUsuarios();
+
+  const usuarioExiste = usuarios.find(
+    user => user.email === novoUsuario.email
+  );
+
+  if (usuarioExiste) {
+    throw new Error('Usuário já cadastrado');
+  }
+
+  usuarios.push(novoUsuario);
+
+  await AsyncStorage.setItem(
+    STORAGE_KEYS.USUARIOS,
+    JSON.stringify(usuarios)
+  );
+}
+export async function login(email, senha) {
+  const usuarios = await buscarUsuarios();
+
+  const usuario = usuarios.find(
+    user => user.email === email && user.senha === senha
+  );
+
+  if (!usuario) {
+    throw new Error('Email ou senha inválidos');
+  }
+
+  await AsyncStorage.setItem(
+    STORAGE_KEYS.USUARIO_LOGADO,
+    JSON.stringify(usuario)
+  );
+
+  return usuario;
+}
+
+export async function buscarUsuarioLogado() {
+  const dados = await AsyncStorage.getItem(
+    STORAGE_KEYS.USUARIO_LOGADO
+  );
+
   return dados ? JSON.parse(dados) : null;
 }
 
+export async function logout() {
+  await AsyncStorage.removeItem(
+    STORAGE_KEYS.USUARIO_LOGADO
+  );
+}
+
+
 export async function salvarPosts(posts) {
-  await AsyncStorage.setItem(STORAGE_KEYS.POSTS, JSON.stringify(posts));
+  await AsyncStorage.setItem(
+    STORAGE_KEYS.POSTS,
+    JSON.stringify(posts)
+  );
 }
 
 export async function buscarPosts() {
-  const dados = await AsyncStorage.getItem(STORAGE_KEYS.POSTS);
+  const dados = await AsyncStorage.getItem(
+    STORAGE_KEYS.POSTS
+  );
+
   return dados ? JSON.parse(dados) : [];
 }
 
 export async function limparTudo() {
-  await AsyncStorage.removeItem(STORAGE_KEYS.USUARIO);
-  await AsyncStorage.removeItem(STORAGE_KEYS.POSTS);
+  await AsyncStorage.clear();
 }
